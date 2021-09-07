8c782d4f732122866a6450718c2bf3284e08d74c
robertrapao <robertrapalo@gmail.com>
2021-09-07 17:04:10 -0600

    fix

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ app.get('/', (req, res) => {
     res.redirect('/index.html')
 });
 
+app.get('/obtener-usuario', (req, res) => {
+    let usuario = [{
+            id: 1,
+            descripcion: 'prueba'
+        },
+        {
+            id: 1,
+            descripcion: 'prueba'
+        }
+    ]
+    res.status(200).json(usuario)
+});
+
 
 var port = process.env.PORT || 3000;
 
