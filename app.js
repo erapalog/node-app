30700f7255c68de1cc7f185d72a2c5f6515a438b
robertrapao <robertrapalo@gmail.com>
2021-09-07 16:33:59 -0600

    fix

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express();
+
 app.use(express.static(__dirname + '/fronted'));
+
+console.log(process.env);
 
 app.get('/', (req, res) => {
     res.redirect('/index.html')
